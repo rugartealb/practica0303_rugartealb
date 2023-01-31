b95393d1c95606ed20654b42b00aea2cb5f2f57c
Ramón Ugarte Albizu <rugartealb@educacion.navarra.es>
2023-01-31 17:48:45 +0100

    clase roca añadida

--- arkanoid.py
+++ arkanoid.py
@@ -1,42 +1,57 @@
 import time
 import pygame
 from pygame import mixer
-from brick import Brick
+from brick import Brick, Rock
 
-#Instantiate mixer
+
+# Inicializo theme music
 mixer.init()
-
-#Load audio file
-mixer.music.load('wish.mp3')
-
-#Set preferred volume
-mixer.music.set_volume(10)
-
-#Play the music
-mixer.music.play()
+theme_sound = mixer.music.load('wish.mp3')
+theme_sound = mixer.music.set_volume(10)
+theme_sound = mixer.music.play()
 
 pygame.init()
 ventana = pygame.display.set_mode((640,480))
 pygame.display.set_caption("Arkanoid")
 
+# Crea el objeto ball, y obtengo su rectángulo
 ball = pygame.image.load("ball.png")
 ball_rect = ball.get_rect()
 ball_speed = 4
-ball_dir = [1, 1]
-ball_rect.move_ip(0,0)
+ball_dir = [1, -1]
+ball_rect.move_ip(240,350)
 
 # Crea el objeto bate, y obtengo su rectángulo
 bate = pygame.image.load("bate.png")
 bate_rect = bate.get_rect()
 bate_speed = 3
 bate_prev_dir = -1
+bate_rect.move_ip(240,420)
 
+# Creo los ladrillos y los posiciono en cuadrícula
+lista_ladrillos = []
+for posx in range(16):
+    for posy in range(4):
+        lista_ladrillos.append(Brick(40*posx, 45*posy, "brick.png"))
+
+# Creo las rocas y las posiciono
+lista_rocas = []
+lista_rocas.append(Rock(0, 180, "rock.png"))
+lista_rocas.append(Rock(40, 180, "rock.png"))
+lista_rocas.append(Rock(560, 180, "rock.png"))
+lista_rocas.append(Rock(600, 180, "rock.png"))
+
+# Cargo imagen y sonido de gameover
 gameover = pygame.image.load("gameover.png")
 gameover_rect = gameover.get_rect()
-gameover_rect.move_ip(0,0)
+gameover_rect.move_ip(0, 0)
+gameover_sound = mixer.Sound('gameover.mp3')
 
-# Pongo el bate en la parte inferior de la pantalla
-bate_rect.move_ip(240,400)
+# Cargo imagen y sonido de winner
+winner = pygame.image.load("winner.png")
+winner_rect = winner.get_rect()
+winner_rect.move_ip(0, 0)
+winner_sound = mixer.Sound('winner.mp3')
 
 jugando = True
 while jugando:
@@ -44,7 +59,7 @@ while jugando:
         if event.type == pygame.QUIT:
             jugando = False
 
-    # Compruebo si se ha pulsado alguna tecla y si se ha pulsado previamente, para acelerar la barra
+    # Compruebo si se ha pulsado alguna tecla y si se ha pulsado previamente, acelero el bate
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and bate_rect.left > 0:
         if bate_prev_dir == -1:
@@ -70,6 +85,17 @@ while jugando:
         if ball_speed < 10:
             ball_speed = ball_speed + 0.5
 
+    # Compruebo si hay colisión con ladrillos
+    for ladrillo in lista_ladrillos:
+        if ball_rect.colliderect(ladrillo.rect):
+            ball_dir[1] = -ball_dir[1]
+            lista_ladrillos.remove(ladrillo)
+
+    # Compruebo si hay colisión con rocas
+    for roca in lista_rocas:
+        if ball_rect.colliderect(roca.rect):
+            ball_dir[1] = -ball_dir[1]
+
     # Muevo la bola
     ball_rect = ball_rect.move([ball_dir[0]*ball_speed, ball_dir[1]*ball_speed])
     if ball_rect.left < 0 or ball_rect.right > ventana.get_width():
@@ -86,15 +112,29 @@ while jugando:
     # Dibujo el bate
     ventana.blit(bate, bate_rect)
 
+    # dibujo los ladrillos
+    for ladrillo in lista_ladrillos:
+        ventana.blit(ladrillo.image, ladrillo.rect)
+
+    # dibujo las rocas
+    for roca in lista_rocas:
+        ventana.blit(roca.image, roca.rect)
+
+    # Compruebo si quedan ladrillos
+    if len(lista_ladrillos) == 0:
+        ventana.blit(winner, winner_rect)
+        winner_sound.play()
+        jugando = False
+
     # Compruebo si toca el fondo
     if ball_rect.bottom > ventana.get_height():
         ventana.blit(gameover, gameover_rect)
-        mixer.music.load('gameover.mp3')
-        mixer.music.set_volume(10)
-        mixer.music.play()
+        gameover_sound.play()
         jugando = False
 
+    # Refresco de pantalla
     pygame.display.flip()
+    # Set framerate
     pygame.time.Clock().tick(60)
 
 time.sleep(3)
--- brick.py
+++ brick.py
@@ -1,5 +1,11 @@
-class Brick:
-    def __init__(self, pos_x, pos_y):
-        self.__posx = pos_x
-        self.__posy = pos_y
+import pygame
 
+
+class Brick:
+    def __init__(self, pos_x, pos_y, image_path):
+        self.image = pygame.image.load(image_path)
+        self.rect = self.image.get_rect()
+        self.rect.move_ip(pos_x, pos_y)
+
+class Rock(Brick):
+    pass
